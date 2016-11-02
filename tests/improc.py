9f3f136511a27e22f2d8ad9f2e65b8e222a7ccae
qed <qed@dhcp-wifi-8021x-155-41-44-59.bu.edu>
2016-11-02 13:42:52 -0400

    completed the fread program

--- tests/improc.py
+++ tests/improc.py
@@ -1,4 +1,27 @@
 from PIL import Image
+
+def indSum( x ) :
+	sig = 0;
+	for i in range( 3 ):
+		if( x[i] ):
+			sig += i+1;
+			
+	return sig;
+
+def getRGB( x ):
+	sig = indSum( x );
+	if( sig == 0 ):
+		return "Blk";
+	elif( sig == 6 ):
+		return "W";
+	elif( sig == 1):
+		return "R";
+	elif( sig == 2 ):
+		return "G";
+	elif( sig == 3 ):
+		return "Blu";
+
+
 newImg = "new.png";
 
 img = Image.open('simg.png');
@@ -6,8 +29,9 @@ img.show();
 arr = img.load();
 
 ( x, y ) = img.size;
+#x - width, y - height
 print( "the image is " + str( x ) + " by " + str( y ) + "." );
 for i in range ( x ):
 	for j in range( y ):
-		print( arr[i, j], end='' );
+		print( getRGB( arr[j,i] ) , end=' ' );
 	print( "" );	
