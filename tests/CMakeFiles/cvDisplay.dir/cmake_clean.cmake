e6e6d04e10c9b89fbbdcec70b7d4403af4c46597
qed <qed@dhcp-resnet-168-122-133-204.bu.edu>
2016-11-11 14:39:20 -0500

    finally installed opencv, make a basic program

---
+++ tests/CMakeFiles/cvDisplay.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cvDisplay.dir/cvDisplay.cpp.o"
+  "cvDisplay.pdb"
+  "cvDisplay"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cvDisplay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ tests/CMakeFiles/cvMod.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cvMod.dir/cvMod.cpp.o"
+  "cvMod.pdb"
+  "cvMod"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cvMod.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
